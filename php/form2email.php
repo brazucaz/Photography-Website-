c7f86f16a770ab0f3b8e1bd937c2a84687fe3cf1
csellers15 <csellers18@yahoo.com>
2018-02-27 18:02:53 -0500

    fixed spacing issues

--- php/form2email.php
+++ php/form2email.php
@@ -1,15 +1,12 @@
 <?php
 
   // Variables using POST Array to hold email content
-
   $name = $_POST['visitorName'];//Holds the users name
   $email = $_POST['visitorEmail'];//Holds the users email address
   $message = $_POST=['message'];//Holds the message that was written by the user
-
   /*
   Function that checks form validity
   */
-
   function isValidForm() {
     $name = $email = $message = "";//Setting variables to blank value
 
@@ -23,38 +20,28 @@
       $data = htmlspecialchars($data);
       return $data;
     }
-
     /*
     If the server request method is POST, then put the form data
     through the validData function
     */
-
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $name = validData(global $name);
       $email = validData(global $email);
       $message = validData(global $message);
     }
-
-
   }
-
   /*
   Function that creates and sends email
   note, form must have valid inputs
   */
-
   function createAndSend() {
-
     //this variable is just aggregation of the data to create an email message
     $sentMail =
-
     "Hi there! You have recieved an email from " + global$name +
     "\n" + "EMAIL: " + global$email + "\n" +
     "MESSAGE: " + global$message;
 
     //Sending out the message
     mail("algchoo@gmail.com", "TestEmail", $sentMail);
-
   }
-
  ?>

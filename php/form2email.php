6cf787f2b693af88209252393648127c0bb2026f
csellers15 <csellers18@yahoo.com>
2018-03-02 18:16:46 -0500

    testing a new form background code (may not work, untested)

--- php/form2email.php
+++ php/form2email.php
@@ -3,37 +3,21 @@
   $email = $_POST['visitorEmail'];//Holds the users email address
   $message = $_POST=['message'];//Holds the message that was written by the user
 
-  /*****************************************************************************
-  Function that checks form validity
-  *****************************************************************************/
-  function isValidForm() {
-    $name = $email = $message = "";//Setting variables to blank value
-    /***************************************************************************
-    Manipulates data when submitting form so that
-    the form is protected from Cross-site scripting
-    ***************************************************************************/
-    function validData($data) {
-      $data = trim($data);
-      $data = stripslashes($data);
-      $data = htmlspecialchars($data);
-      return $data;
-    }
-    /***************************************************************************
-    If the server request method is POST, then put the form data
-    through the validData function
-    ***************************************************************************/
+    $name = " ";
+    $email = " ";
+    $message = " ";
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $name = validData(global $name);
       $email = validData(global $email);
       $message = validData(global $message);
     }
-  }
-  /*****************************************************************************
-  Function that creates and sends email
-  note, form must have valid inputs
-  *****************************************************************************/
-  function createAndSend() {
-    //this variable is just aggregation of the data to create an email message
+
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+
     $sentMail =
     "Hi there! You have recieved an email from " + global$name +
     "\n" + "EMAIL: " + global$email + "\n" +
@@ -41,5 +25,4 @@
 
     //Sending out the message
     mail("algchoo@gmail.com", "TestEmail", $sentMail);
-  }
  ?>

78c67ca92e628e2144b0e39e7f79bd99292fa8b7
csellers15 <csellers18@yahoo.com>
2018-03-03 21:48:46 -0600

    proper indentation

--- php/formtest.php
+++ php/formtest.php
@@ -1,54 +1,48 @@
 <?php
-if(isset($_POST['email'])) {
+  if(isset($_POST['email'])) {
+        $email_to = "sellersc@mail.gvsu.edu";
+        $email_subject = "Business inquiry";
 
-      $email_to = "you@yourdomain.com";
-      $email_subject = "Your email subject line";
+        if(!isset($_POST['name']) || !isset($_POST['email']) ||!isset($_POST['message']))
+            died('We are sorry, but there appears to be a problem with the form you submitted.');
 
-      function died($error) {
-          echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-          echo "These errors appear below.<br /><br />";
-          echo $error."<br /><br />";
-          echo "Please go back and fix these errors.<br /><br />";
-          die();
-      }
+        $first_name = $_POST['name'];
+        $email_from = $_POST['email'];
+        $comments = $_POST['message'];
 
-      if(!isset($_POST['name']) ||
-          !isset($_POST['email']) ||
-          !isset($_POST['message'])) {
-          died('We are sorry, but there appears to be a problem with the form you submitted.');
-      }
-      $first_name = $_POST['name']; // required
-      $email_from = $_POST['email']; // required
-      $comments = $_POST['message']; // required
+        $error_message = "";
+        $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+        $string_exp = "/^[A-Za-z .'-]+$/";
 
-      $error_message = "";
-      $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-      $string_exp = "/^[A-Za-z .'-]+$/";
+      if(!preg_match($email_exp,$email_from))
+        $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+      if(!preg_match($string_exp,$name))
+        $error_message .= 'The Name you entered does not appear to be valid.<br />';
+      if(strlen($messsage) < 2)
+        $error_message .= 'The Message you entered do not appear to be valid.<br />';
+      if(strlen($error_message) > 0)
+        died($error_message);
 
-    if(!preg_match($email_exp,$email_from))
-      $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-    if(!preg_match($string_exp,$name))
-      $error_message .= 'The Name you entered does not appear to be valid.<br />';
-    if(strlen($messsage) < 2)
-      $error_message .= 'The Message you entered do not appear to be valid.<br />';
-    if(strlen($error_message) > 0)
-      died($error_message);
+        $email_message = "Form details below.\n\n";
+        $email_message .= "Name: ".clean_string($name)."\n";
+        $email_message .= "Email: ".clean_string($email_from)."\n";
+        $email_message .= "Message: ".clean_string($message)."\n";
 
-      $email_message = "Form details below.\n\n";
+    // create email headers
+    $headers = 'From: '.$email_from."\r\n".'Reply-To: '.$email_from."\r\n" .'X-Mailer: PHP/'.phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
 
-      function clean_string($string) {
-        $bad = array("content-type","bcc:","to:","cc:","href");
-        return str_replace($bad,"",$string);
-      }
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
 
-      $email_message .= "Name: ".clean_string($name)."\n";
-      $email_message .= "Email: ".clean_string($email_from)."\n";
-      $email_message .= "Messaage: ".clean_string($message)."\n";
-
-  // create email headers
-  $headers = 'From: '.$email_from."\r\n".
-  'Reply-To: '.$email_from."\r\n" .
-  'X-Mailer: PHP/' . phpversion();
-  @mail($email_to, $email_subject, $email_message, $headers);
-}
+    function died($error) {
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+  }
 ?>

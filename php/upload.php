6a2c5c745eb6bb3a2e02f76d03a0d5918ec15e27
csellers15 <csellers18@yahoo.com>
2018-03-05 17:49:58 -0500

    minor changes

--- php/upload.php
+++ php/upload.php
@@ -31,8 +31,7 @@ if ($_FILES["fileToUpload"]["size"] > 5000000) {
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"  && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br />";
     $uploadOk = 0;
 }
